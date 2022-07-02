5b9da47794f97321555a92bedd972ed2a6ef5be4
Omri Green <omri.green1@gmail.com>
2022-07-02 16:57:03 +0300

    feat: add react router functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import MainContent from './components/main/MainContent';
 
 const App = () => {
-  return <MainContent />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainContent />} />
+    </Routes>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// react router setup
+import { BrowserRouter } from 'react-router-dom';
+
 // providing redux store to app
 import store from './store/store';
 import { Provider } from 'react-redux';
@@ -10,6 +13,8 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
