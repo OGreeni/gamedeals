aa7eca9f53a6627e5d368c524599e97741f055dc
Omri Green <omri.green1@gmail.com>
2022-07-13 17:21:42 -0400

    fix: add index page to navbar

--- src/components/navigation/MainNav.js
+++ src/components/navigation/MainNav.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const MainNav = () => {
   return (
     <nav>
+      <Link to="/">Home</Link>
       <Link to="about">About</Link>
       <Link to="login">Login</Link>
       <Link to="signup">Signup</Link>
