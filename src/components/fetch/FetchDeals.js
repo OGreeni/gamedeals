3a40ebe2f86afd6be2253f9e8965cc9e2e7060c3
Omri Green <omri.green1@gmail.com>
2022-07-21 22:35:15 -0400

    feat: add pagination buttons (nonfunctional)

--- src/components/fetch/FetchDeals.js
+++ src/components/fetch/FetchDeals.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Spinner, Row } from 'react-bootstrap';
+import { Spinner, Row, Pagination } from 'react-bootstrap';
 
 import DealCard from '../DealCard';
+import './FetchDeals.css';
 
 const FetchDeals = () => {
   const [dealsArray, setDealsArray] = useState([]);
@@ -11,6 +12,7 @@ const FetchDeals = () => {
   const formUserInput = useSelector((state) => state.deals.userInput);
   const userId = useSelector((state) => state.auth.userId);
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const uiTheme = useSelector((state) => state.theme.uiTheme);
   const saveDealUpdater = useSelector((state) => state.deals.saveDealUpdater);
 
   useEffect(() => {
@@ -45,16 +47,33 @@ const FetchDeals = () => {
   }
   if (savedDealsArray) {
     console.log(savedDealsArray);
-    return dealsArray.map((deal) => (
-      <DealCard
-        savedDealsArray={savedDealsArray}
-        cheapsetDealId={deal.cheapestDealID}
-        cheapest={deal.cheapest}
-        external={deal.external}
-        thumb={deal.thumb}
-        key={deal.gameID}
-      />
-    ));
+    return (
+      <>
+        {dealsArray.map((deal) => (
+          <DealCard
+            savedDealsArray={savedDealsArray}
+            cheapsetDealId={deal.cheapestDealID}
+            cheapest={deal.cheapest}
+            external={deal.external}
+            thumb={deal.thumb}
+            key={deal.gameID}
+          />
+        ))}
+        <Row className="justify-content-center">
+          <Pagination
+            size="lg"
+            className={
+              uiTheme === 'light'
+                ? 'pagination-container'
+                : 'pagination-container-dark'
+            }
+          >
+            <Pagination.Prev className="pagination-btn" />
+            <Pagination.Next className="pagination-btn" />
+          </Pagination>
+        </Row>
+      </>
+    );
   }
 };
 
